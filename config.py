e666dbc98eeb1b3daaaa6efb1fb84f04467895af
Troy-bailan-King <97757033+Troy-bailan-King@users.noreply.github.com>
2024-12-03 10:42:08 -0500

    delete

--- config.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class Config:
-    MONGO_URI = "mongodb://localhost:27017/indexDB"
-
-def get_db():
-    client = MongoClient(Config.MONGO_URI)
-    db = client['indexDB']
-    return db
